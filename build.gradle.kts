00fb72fd88884c7beaa2870e1d070e6858f9d2f1
Joshua Dean Küpper <admin@joshua-kuepper.de>
2022-01-20 18:02:27 +0100

    bump version number for release to 3.0.0
    Signed-off-by: Joshua Dean Küpper <admin@joshua-kuepper.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val mavenRepositoryToken: String by project
 
 // provide general GAV coordinates
 group = "net.justchunks"
-version = "2.0.1-SNAPSHOT"
+version = "3.0.0"
 description = "Agones Java Client SDK"
 
 // define the grpc versions for the build
