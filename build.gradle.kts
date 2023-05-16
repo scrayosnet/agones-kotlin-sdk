55205a92f18c76d3cea69ae7d6326c4deac85cac
Joshua Dean Küpper <admin@joshua-kuepper.de>
2023-05-16 16:20:39 +0200

    bump version number for release to 4.0.0
    Signed-off-by: Joshua Dean Küpper <admin@joshua-kuepper.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ val coroutinesVersion: String by project
 
 // provide general GAV coordinates
 group = "net.justchunks"
-version = "4.0.0-SNAPSHOT"
+version = "4.0.0"
 description = "Agones Client SDK (Kotlin/Java)"
 
 // hook the plugins for the builds
