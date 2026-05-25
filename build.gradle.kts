eddb179bfe3c03e09e3a87fb88ac3663449d6047
Joshua Dean Küpper <admin@joshua-kuepper.de>
2026-05-25 12:20:35 +0200

    bump version for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import java.net.URI
 
 // provide general GAV coordinates
 group = "net.scrayos"
-version = "6.0.0-SNAPSHOT"
+version = "6.0.0"
 description = "Agones Client SDK (Kotlin/Java)"
 
 // hook the plugins for the builds
